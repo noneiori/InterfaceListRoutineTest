093074d001c31a1641c91817525798978b7f7070
noneiori <noneiori@gmail.com>
2023-09-13 22:20:21 +0800

    加入專案檔案。

---
+++ InterfaceListRoutineTest/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterfaceListRoutineTest
+{
+    public class Cat : IAnimal,ICloneable
+    {
+        public int ID { get; set; }
+
+        public Cat Child { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ InterfaceListRoutineTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace InterfaceListRoutineTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Queue<IAnimal> animals = new Queue<IAnimal>();
+
+            List<IAnimal> animals_list = new List<IAnimal>();
+            for (int i = 0; i < 5; i++)
+            {
+                animals.Enqueue(new Cat() { ID = i, Child = new Cat() { ID = i } });
+            }            
+
+            for (int i = 0; i < 10; i++)
+            {
+                Cat cat = (Cat)animals.Dequeue();
+                Cat copycat = (Cat)cat.Clone();
+                copycat.ID = i;
+                copycat.Child.ID = i;
+                animals_list.Add(copycat);
+                animals.Enqueue(copycat);
+            }
+        }
+    }
+
+    public interface IAnimal
+    {
+        int ID { get; set; }
+    }
+}
