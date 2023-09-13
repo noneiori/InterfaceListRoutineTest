c961eb05177e26010f6f4742e8a377cf73aa04e3
noneiori <noneiori@gmail.com>
2023-09-13 22:29:10 +0800

    加入automapper複製測試

--- InterfaceListRoutineTest/UnitTest1.cs
+++ InterfaceListRoutineTest/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
@@ -22,6 +23,35 @@ namespace InterfaceListRoutineTest
             {
                 Cat cat = (Cat)animals.Dequeue();
                 Cat copycat = (Cat)cat.Clone();
+                copycat.ID = i;
+                copycat.Child.ID = i;
+                animals_list.Add(copycat);
+                animals.Enqueue(copycat);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Queue<IAnimal> animals = new Queue<IAnimal>();
+
+            List<IAnimal> animals_list = new List<IAnimal>();
+            for (int i = 0; i < 5; i++)
+            {
+                animals.Enqueue(new Cat() { ID = i, Child = new Cat() { ID = i } });
+            }
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Cat, Cat>();
+            });
+            var mapper = config.CreateMapper();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Cat cat = (Cat)animals.Dequeue();
+                Cat copycat = new Cat();
+                mapper.Map(cat, copycat);
                 copycat.ID = i;
                 copycat.Child.ID = i;
                 animals_list.Add(copycat);
